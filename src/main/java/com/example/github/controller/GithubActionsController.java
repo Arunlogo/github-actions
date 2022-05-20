219c2d59486476b3487beb64f0c0a105e4d3389d
Arun <33146069+Arunlogo@users.noreply.github.com>
2022-05-20 17:46:00 +0530

    Update GithubActionsController.java

--- src/main/java/com/example/github/controller/GithubActionsController.java
+++ src/main/java/com/example/github/controller/GithubActionsController.java
@@ -17,7 +17,7 @@ public class GithubActionsController {
 	@Autowired
 	Actions actions;
 	
-	@GetMapping("/get")
+	@GetMapping("/gett")
 	public Optional<String> getGreetings() {
 		return Optional.of("Hi, Welcome!!");
 	}
